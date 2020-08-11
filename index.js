b44e91e27f64979eff6c2503a11570d045665ab2
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-08-11 13:43:22 -0700

    add call to location iq

--- index.js
+++ index.js
@@ -5,41 +5,59 @@ const geoData = require ('./data/geo.js');
 const weatherData = require('./data/weather.js')
 const app = express();
 const port = process.env.PORT || 3000;
+import request from 'superagent';
 
 app.use(cors());
 
 app.use(express.static('public'));
 
-function getWeather(lat, lon) {
-        const data = weatherData.data;
+const { GEOCODE_API_KEY } = process.env;
 
-        const forecastArray = data.map((weatherItem) => {
-            return {
-                forecast: weatherItem.weather.description,
-                time: new Date(weatherItem.ts * 1000),
-            };
-        });
-        return forecastArray;
+
+function getWeather(lat, lon) {
+    const data = weatherData.data;
+    
+    const forecastArray = data.map((weatherItem) => {
+        return {
+            forecast: weatherItem.weather.description,
+            time: new Date(weatherItem.ts * 1000),
+        };
+    });
+    return forecastArray;
 }
 
 
-function getLatLong(cityName) {
-        const city = geoData[0];
-
-        return {
+async function getLatLong(cityName) {
+    const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${cityName}&format=json`);
+    
+    const city = response[0];
+    return {
             formatted_query: city.display_name,
             latitude: city.lat,
             longitude: city.lon
         };
 }
 
-app.get('/location', (req, res) => {
+app.get('/location', async(req, res) => {
     const userInput = req.query.search;
 
-    const mungeData = getLatLong(userInput);
+    const mungeData = await getLatLong(userInput);
     res.json(mungeData);
     
 });
+
+// app.get('/chars', (req, res) => {
+//         const response = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=char');
+
+//         const pokemon = response.body.results;
+
+//         const names = pokemon.map((poke) => {
+//             return poke.pokemon;
+        
+            
+//         });
+//         res.json(names);
+// });
 
 app.get('/weather', (req, res) => {
   const userLat = req.query.latitude;
@@ -53,3 +71,7 @@ app.get('/weather', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
+
+module.exports = {
+    app
+}
